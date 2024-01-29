3f8d4c4d17bc587f47f8e9ef56b024e347523875
alidakik <afdk2003@gmail.com>
2024-01-29 18:58:06 +0330

    fix a bug

--- VGA.v
+++ VGA.v
@@ -41,8 +41,8 @@ horizontal_counter VGA_Horiz (clk_25MHz, enable_V_Counter, H_Count_Value);
 vertical_counter VGA_Verti (clk_25MHz, enable_V_Counter, V_Count_Value);
 
 // outputs
-assign Hsynq = (H_Count_Value < 96) ? 1'bl:1'b0;
-assign Vsynq = (V_Count_Value < 2) ? l'bl:1'b0;
+assign Hsynq = (H_Count_Value < 96) ? 1'b1:1'b0;
+assign Vsynq = (V_Count_Value < 2) ? 1'b1:1'b0;
 // colors
 assign Red = (H_Count_Value < 784 && H_Count_Value > 143 && V_Count_Value < 515 && V_Count_Value>34) ? 4'hF:4'h0;
 assign Green = (H_Count_Value < 784 && H_Count_Value > 143 && V_Count_Value < 515 && V_Count_Value>34) ? 4'hF:4'h0;
